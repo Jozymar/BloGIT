45292dad0206c3b9aa007ad178a7244ca5aeff66
Jozymar <jozimarsoares.js@gmail.com>
2016-09-26 21:36:45 -0300

    Segunda Postagem Adicionada

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+post03.onclick = function () {
+  frames['iframe'].location.href = 'paginas/post03.html'
+}
+
+post04.onclick = function () {
+  frames['iframe'].location.href = 'paginas/post04.html'
+}
+
 post05.onclick = function () {
   frames['iframe'].location.href = 'paginas/post05.html'
 }
