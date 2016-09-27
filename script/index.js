e1dfc805fe5df9773162488bb2ac142cc8d41401
Matheus Ramyres <matheus63360@gmail.com>
2016-09-27 14:31:11 -0300

    Quinta postagem adicionada e erro dos botões do final da pagina corrigidos

--- script/index.js
+++ script/index.js
@@ -1,21 +1,46 @@
 post01.onclick = function () {
-  frames['iframe'].location.href = 'paginas/post01.html'
+  frames['iframe'].location.href = 'paginas/post01.html';
+    document.getElementById("bt01").style.backgroundColor = "black";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
 }
 
 post02.onclick = function () {
-  frames['iframe'].location.href = 'paginas/post02.html'
+  frames['iframe'].location.href = 'paginas/post02.html';
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "black";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
 }
 
 post03.onclick = function () {
-  frames['iframe'].location.href = 'paginas/post03.html'
+  frames['iframe'].location.href = 'paginas/post03.html';
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "black";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
 }
 
 post04.onclick = function () {
-  frames['iframe'].location.href = 'paginas/post04.html'
+  frames['iframe'].location.href = 'paginas/post04.html';
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "black";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
 }
 
 post05.onclick = function () {
-  frames['iframe'].location.href = 'paginas/post05.html'
+  frames['iframe'].location.href = 'paginas/post05.html';
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "black";
 }
 
 bt01.onclick = function(){
