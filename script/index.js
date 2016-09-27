f73d43928d9093f1c3e1c464374c3ee7ba632f1b
Matheus Ramyres <matheus63360@gmail.com>
2016-09-27 12:09:31 -0300

    Botões do final da página configurados

--- script/index.js
+++ script/index.js
@@ -1,3 +1,7 @@
+post01.onclick = function () {
+  frames['iframe'].location.href = 'paginas/post01.html'
+}
+
 post02.onclick = function () {
   frames['iframe'].location.href = 'paginas/post02.html'
 }
@@ -14,3 +18,42 @@ post05.onclick = function () {
   frames['iframe'].location.href = 'paginas/post05.html'
 }
 
+bt01.onclick = function(){
+    document.getElementById("bt01").style.backgroundColor = "black";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
+}
+
+bt02.onclick = function(){
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "black";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
+}
+
+bt03.onclick = function(){
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "black";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
+}
+
+bt04.onclick = function(){
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "black";
+    document.getElementById("bt05").style.backgroundColor = "#ddd";
+}
+
+bt05.onclick = function(){
+    document.getElementById("bt01").style.backgroundColor = "#ddd";
+    document.getElementById("bt02").style.backgroundColor = "#ddd";
+    document.getElementById("bt03").style.backgroundColor = "#ddd";
+    document.getElementById("bt04").style.backgroundColor = "#ddd";
+    document.getElementById("bt05").style.backgroundColor = "black";
+}
